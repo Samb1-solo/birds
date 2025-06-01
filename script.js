46e6421cca151c6df7d4703f6d3061d057e9890a
Samb1-solo <153252903+Samb1-solo@users.noreply.github.com>
2025-06-01 01:49:29 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -8,24 +8,25 @@ const passwordInput = document.getElementById("password");
 
 const output = document.getElementById("login-page");
 
-const addLocalBirds = document.getElementById("travelling-header");
 
-let username = "apple";
+let username = "orange";
 let password = "juice";
 
 const checkInput = ()=>{
        if (usernameInput.value === username && passwordInput.value === password){
         sessionStorage.setItem("loggedIn","true");
         // Set a cookie
-        document.cookie = "loggedIn=true; path=/; max-age=86400"; // Expires in 1 day
+        document.cookie = "loggedIn=true; path=/; max-age=3600"; // Expires in 1 day
 
         output.innerHTML = 
-        '<p id="result">You are logged in <a href=" Birds.html"> Click here. </a></p>' ;
-        addLocalBirds.innerHTML += '<div class="header-text"><a href="addLocalBirds.html"> Add Bird </a></div>';
-       }
+        '<p id="result">You are logged in <a href=" birds-page.html"> Click here. </a> Your login will be valid for an hour.</p>'
+        + '<div id="addButton"><label>Click on the button to contribute</label><button type="button" onClick="addBird()">Add</button></div>';
+        
+       
+        }
             
         else{
-            output.innerHTML = '<p id="result">Login failed <a href="login.html"> Click here </a></p>';
+            output.innerHTML = '<p id="result">Login failed <a href="login-page.html"> Click here </a></p>';
         }
 
      };
@@ -41,7 +42,8 @@ checkInput()
 
 if (sessionStorage.getItem("loggedIn") === "true" || getCookie("loggedIn") === "true") {
     console.log("User is still logged in!");
-    output.innerHTML = '<p>You are still logged in. <a href="Birds.html">Go to Birds Page</a></p>';
+    output.innerHTML = '<p style="display:inline;">You are still logged in. <a href="birds.html">Go to Birds Page</a> Your login will be valid for an hour since you have logged in.</p>'
+     + '<div id="addButton"><label>Click on the button to contribute</label><button type="button" onClick="addBird()">Add</button></div>';
 }
 
 
@@ -53,3 +55,19 @@ function getCookie(name) {
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop().split(";").shift();
 }
+
+function logout(){
+    document.cookie = "loggedIn=; path=/; max-age=0";
+    localStorage.removeItem("authToken");
+    sessionStorage.clear();
+    alert("You have succesfully logged out!");
+    window.location.href="/index.html";
+}
+function addBird(){
+    alert("Be mindful of what you are adding. This is a personal project!");
+    window.location.href="/add-bird-page.html";
+}
+
+document.getElementById("cancel-bird").addEventListener("click", () => {
+    document.querySelector("form").reset();
+});
